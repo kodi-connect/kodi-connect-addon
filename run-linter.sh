aa3ddc9592801ff5b9656d0a8a77907671eccddd
Tomas Kislan <kislan.tomas@gmail.com>
2018-02-04 17:31:12 +0100

    Fixed flake invocation

--- run-linter.sh
+++ run-linter.sh
@@ -3,5 +3,5 @@
 pylint resources/connect service.py plugin.py
 pylint --disable=duplicate-code,relative-import,invalid-name,protected-access tests/
 
-flake8 --ignore=E402 service.py plugin.py
+flake8 --ignore=E402,E302,E305 service.py plugin.py
 flake8 resources/connect tests
