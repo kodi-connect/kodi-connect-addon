0898b4b207cc31c92defac46c863285b923a69f6
Tomas Kislan <kislan.tomas@gmail.com>
2018-09-06 00:06:58 +0200

    Fix lint errors

--- run-linter.sh
+++ run-linter.sh
@@ -4,4 +4,4 @@ pylint resources/connect service.py plugin.py
 pylint --disable=duplicate-code,relative-import,invalid-name,protected-access tests/
 
 flake8 --ignore=E402,E302,E305,E722 service.py plugin.py
-flake8 --ignore=E722 resources/connect tests
+flake8 --ignore=E302,E305,E722 resources/connect tests
