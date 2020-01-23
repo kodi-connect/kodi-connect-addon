5d39016275256980de6f19d3c84216c55ce517a0
Tomas Kislan <kislan.tomas@gmail.com>
2020-01-23 10:44:05 +0100

    Fix lint error

--- resources/connect/utils.py
+++ resources/connect/utils.py
@@ -25,7 +25,7 @@ def encode(string):
 def strip_accents(text):
     try:
         text = unicode(text, 'utf-8')
-    except (TypeError, NameError): # unicode is a default on python 3
+    except (TypeError, NameError):  # unicode is a default on python 3
         pass
     text = unicodedata.normalize('NFD', text)
     text = text.encode('ascii', 'ignore')
