5de5e65259f2f90bc02b59328f4c12a535c8ee45
Tomas Kislan <kislan.tomas@gmail.com>
2021-03-07 18:55:14 +0000

    Make install-dependencies script compatible with python 3

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -18,14 +18,14 @@ venv_libs=$(file "${tmpdir}/lib/python"*"/site-packages" | cut -d: -f1)
 
 ls -l "${venv_libs}"
 
-cp -r "${venv_libs}/six.py" "${lib_dir}"
-cp    "${venv_libs}/singledispatch.py" "${venv_libs}/singledispatch_helpers.py" "${lib_dir}"
-cp    "${venv_libs}/backports_abc.py" "${lib_dir}"
+cp -r "${venv_libs}/six.py" "${lib_dir}" || true
+cp    "${venv_libs}/singledispatch" "${lib_dir}" || true
+cp    "${venv_libs}/backports_abc.py" "${lib_dir}" || true
+cp -r "${venv_libs}/concurrent" "${lib_dir}" || true
 rm -r "${venv_libs}/tornado/test"
 find  "${venv_libs}/tornado" -name '*.so' -exec rm -v "{}" \;
 cp -r "${venv_libs}/tornado" "${lib_dir}"
 cp -r "${venv_libs}/fuzzywuzzy" "${lib_dir}"
-cp -r "${venv_libs}/concurrent" "${lib_dir}"
 cp    "${venv_libs}/ngram.py" "${lib_dir}"
 
 ls -l "${lib_dir}"
